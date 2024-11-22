d0ac2ec4dbf984828d0385ddf1a2aaf6b1349644
AFIGEL21 <afigel21@gmail.com>
2024-11-22 09:07:22 +0200

    Update Uzdevums 2: Teksta valodas noteikšana.py

--- Uzdevums 2: Teksta valodas noteikšana.py
+++ Uzdevums 2: Teksta valodas noteikšana.py
@@ -1,24 +1,7 @@
 from langdetect import detect
 
-# Funkcija valodas noteikšanai
-def detect_language(text):
-    try:
-        language = detect(text)  # Izmanto langdetect funkciju valodas noteikšanai
-        return language
-    except Exception as e:
-        return f"Error: {str(e)}"  # Ja notiek kļūda, atgriežam kļūdas ziņojumu
+text1 = "Šodien ir saulaina diena."
+text2 = "Today is a sunny day."
 
-while True:
-    # Lietotājs ievada tekstu
-    text = input("Lūdzu, ievadiet tekstu (vai ierakstiet 'beigt', lai izietu): ")
-
-    # Ja lietotājs ievada 'beigt', programma beigsies
-    if text.lower() == 'beigt':
-        print("Programma beidz darbību.")
-        break
-
-    # Noteikt valodu
-    language = detect_language(text)
-
-    # Izvadīt rezultātu
-    print(f"Teksts ir uzrakstīts šādā valodā: {language}")
+print(detect(text1))  # latviešu
+print(detect(text2))  # angļu
